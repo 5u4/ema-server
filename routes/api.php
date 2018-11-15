c434fce2edbfa7ab6df172a791f19e0f196f9c66
ycchou <ycchou@sfu.ca>
2018-11-14 22:11:31 -0800

    Add update transaction end point using transaction id

--- routes/api.php
+++ routes/api.php
@@ -24,6 +24,7 @@ Route::group(['prefix' => 'v1'], function () {
     /* Transaction */
     Route::group(['prefix' => 'transaction', 'middleware' => 'auth'], function () {
        Route::post('/', 'TransactionController@create');
+       Route::put('/{id}', 'TransactionController@update');
        Route::get('/', 'TransactionController@index');
        Route::get('/{id}', 'TransactionController@show');
        Route::delete('/{id}', 'TransactionController@delete');
