9f7b5d3136eb129723b61547ccc1cf6974116a69
jianing <jianings0617@gmail.com>
2018-11-17 17:11:47 -0800

    Added search users endpoint

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -30,7 +30,13 @@ class UserController extends Controller
         return UserResource::collection(collect(User::all()))->response();
     }
 
+    public function search(string $input):JsonResponse
+    {
+        $userList = $this->userService->searchUser($input);
 
+       // dd($userList);
+        return UserResource::collection(collect($userList))->response();
+    }
   
 
 }
